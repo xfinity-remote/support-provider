198059020bf33ee6ba673f014e64a7a4fe7f4024
d2-fnb <dota2fanboyytb@gmail.com>
2024-12-29 17:52:23 +0530

    fix: is_outgoing_only true

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -2094,20 +2094,22 @@ fn is_option_can_save(
 
 #[inline]
 pub fn is_incoming_only() -> bool {
-    HARD_SETTINGS
-        .read()
-        .unwrap()
-        .get("conn-type")
-        .map_or(false, |x| x == ("incoming"))
+    false
+    // HARD_SETTINGS
+    //     .read()
+    //     .unwrap()
+    //     .get("conn-type")
+    //     .map_or(false, |x| x == ("incoming"))
 }
 
 #[inline]
 pub fn is_outgoing_only() -> bool {
-    HARD_SETTINGS
-        .read()
-        .unwrap()
-        .get("conn-type")
-        .map_or(false, |x| x == ("outgoing"))
+    true
+    // HARD_SETTINGS
+    //     .read()
+    //     .unwrap()
+    //     .get("conn-type")
+    //     .map_or(false, |x| x == ("outgoing"))
 }
 
 #[inline]
