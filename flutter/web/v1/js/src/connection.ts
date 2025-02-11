8b270f8127783eb755cb3cec82813fa7d6d6d8f0
d2-fnb <dota2fanboyytb@gmail.com>
2025-02-12 03:13:53 +0530

    feat: custom server (#15)

--- flutter/web/v1/js/src/connection.ts
+++ flutter/web/v1/js/src/connection.ts
@@ -157,7 +157,7 @@ export default class Connection {
 
   async secure(pk: Uint8Array | undefined) {
     if (pk) {
-      const RS_PK = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+      const RS_PK = "PPBhIlUaRLzZqcJCBoQ5PZu3qvXcX261l3hsq60SLm4=";
       try {
         pk = await globals.verify(pk, localStorage.getItem("key") || RS_PK);
         if (pk) {
--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["178.156.155.176"];
+pub const PUBLIC_RS_PUB_KEY: &str = "PPBhIlUaRLzZqcJCBoQ5PZu3qvXcX261l3hsq60SLm4=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
